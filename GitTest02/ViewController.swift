0bde884779268218f2994c599636c6a06252f6f3
D7703_17 <d7703_17@d7703-17ui-imac.local>
2019-03-07 17:26:59 +0900

    change background yellow color

--- GitTest02/ViewController.swift
+++ GitTest02/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
       override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
+            // Do any additsetup after loading the view, typically from a nib.
+            self.view.backgroundColor = UIColor.yellow
       }
 
 
